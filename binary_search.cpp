59d1429b1738b94c94b05d29ef7e80863bbadc18
VAIBHAV CHAUHAN <123004674+UDAAKADAL@users.noreply.github.com>
2024-02-23 11:01:14 +0530

    Update binary_search.cpp

--- binary_search.cpp
+++ binary_search.cpp
@@ -1,94 +1,92 @@
-
 //BINARY SEARCH
 
-// #include<bits/stdc++.h>
-// using namespace std;
-// int binarysearch(int arr[],int size,int target){
-// int start=0;
-// int end=size -1;
-// int mid = start + (end - start) /2;
-// while(start<=end){
-//    int element =arr[mid];
+#include<bits/stdc++.h>
+using namespace std;
+int binarysearch(int arr[],int size,int target){
+int start=0;
+int end=size -1;
+int mid = start + (end - start) /2;
+while(start<=end){
+   int element =arr[mid];
    
-//    if(element == target){
-//       return mid ;
-//    }
-//    else if(target < element){
-//       //go left
-//       end = mid - 1;
-//    }
-//    else {
-//       //go right
-//       start = mid + 1;
+   if(element == target){
+      return mid ;
+   }
+   else if(target < element){
+      //go left
+      end = mid - 1;
+   }
+   else {
+      //go right
+      start = mid + 1;
 
-//    }
-//    mid= start + (end - start) /2;
+   }
+   mid= start + (end - start) /2;
 
-// }
-// //element not found 
-// return -1;
-// }
-// int main(){
-//    int size;
-//    int target;
-//    int arr[100];
-//    cout<<"Enter the size:";
-//    cin>>size;
-//    cout<<"Enter the element:";
-//    for(int i=0;i<size;i++){
-//       cin>>arr[i];
-//    }
-//    cout<<"Enter the value which you want to found:";
-//    cin>>target;
-//    // int arr[]={1,3,4,6,8,9,10,14,16,18,19,20};
-//    // int size=sizeof(arr)/sizeof(int);
-//    // int target =1;
-//    int indexOftarget = binarysearch(arr,size,target);
-//    if(indexOftarget== -1){
-//       cout<<target<<" not found"<<endl;
-//    }
-//    else{
-//       cout<<target<<" found at "<<indexOftarget<<" index"<<endl;
-//    }
+}
+//element not found 
+return -1;
+}
+int main(){
+   int size;
+   int target;
+   int arr[100];
+   cout<<"Enter the size:";
+   cin>>size;
+   cout<<"Enter the element:";
+   for(int i=0;i<size;i++){
+      cin>>arr[i];
+   }
+   cout<<"Enter the value which you want to found:";
+   cin>>target;
+   // int arr[]={1,3,4,6,8,9,10,14,16,18,19,20};
+   // int size=sizeof(arr)/sizeof(int);
+   // int target =1;
+   int indexOftarget = binarysearch(arr,size,target);
+   if(indexOftarget== -1){
+      cout<<target<<" not found"<<endl;
+   }
+   else{
+      cout<<target<<" found at "<<indexOftarget<<" index"<<endl;
+   }
 
-// }
+}
+
+ //pre deine function for binary search
+#include <vector>
+#include <iostream>
+#include<algorithm>
+using namespace std;
+
+int main() {
+vector<int> v{2,3,5,6,7,8};
+if(binary_search(v.begin(),v.end(),6)){
+    cout<<"found!";
+}
+else{
+    cout<<"Not found!";
+}
+  return 0;
+}
 
 
-// //pre deine function for binary search
-// #include <vector>
-// #include <iostream>
-// #include<algorithm>
-// using namespace std;
+//pre deine function for array
+#include <vector>
+#include <iostream>
+#include<algorithm>
+using namespace std;
 
-// int main() {
-// vector<int> v{2,3,5,6,7,8};
-// if(binary_search(v.begin(),v.end(),6)){
-//     cout<<"found!";
-// }
-// else{
-//     cout<<"Not found!";
-// }
-//   return 0;
-// }
-
-
-// //pre deine function for array
-// #include <vector>
-// #include <iostream>
-// #include<algorithm>
-// using namespace std;
-
-// int main() {
-// int arr [ ] = {2,3,5,6,7,8};
-// int size = 6;
-// if(binary_search(arr,arr + size ,8)){
-//     cout<<"found!";
-// }
-// else{
-//     cout<<"Not found!";
-// }
-//   return 0;
-// }
+int main() {
+int arr [ ] = {2,3,5,6,7,8};
+int size = 6;
+if(binary_search(arr,arr + size ,8)){
+    cout<<"found!";
+}
+else{
+    cout<<"Not found!";
+}
+  return 0;
+}
 
 //find occrance using binary search
 
@@ -128,4 +126,33 @@ if(index != -1){
     cout<<"first occurance found at index :"<<index;
 }else
 cout<<"not found ";
+}
+
+
+
+//PIVOT ELEMENT
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int findpivot(vector<int> p){
+    int s =0;
+    int e=p.size()-1;
+    int mid=s+(e-s)/2;
+    while(s<e){
+        if(p[mid] > p[mid+1]){
+            return p[mid];
+        }else if(p[mid-1] > p[mid]){
+            return p[mid -1];
+        }else if(p[s] > p[mid]){
+            e=mid - 1;
+        }else{
+            s = mid + 1;
+        }
+        mid=s+(e-s)/2;
+    }
+}
+int main(){
+    vector<int> p {1,2,3,4,5,6,7,8,1,2};
+    cout<<"found :"<<findpivot(p);
 }
