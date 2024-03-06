c6d7ed4fb06442b6e9a063df623527664d8c8563
VAIBHAV CHAUHAN <123004674+UDAAKADAL@users.noreply.github.com>
2024-03-06 10:51:16 +0530

    Update LinkList.cpp

--- LinkList.cpp
+++ LinkList.cpp
@@ -1,174 +1,174 @@
-// // creating linked list
-// #include <iostream>
-// using namespace std;
+// creating linked list
+#include <iostream>
+using namespace std;
 
-// class Node
-// {
-// public:
-//     int data;
-//     Node *next;
+class Node
+{
+public:
+    int data;
+    Node *next;
 
-//     Node()
-//     {
-//         this->data = 0;
-//         this->next = NULL;
-//     }
+    Node()
+    {
+        this->data = 0;
+        this->next = NULL;
+    }
 
-//     Node(int data)
-//     {
-//         this->data = data;
-//         this->next = NULL;
-//     }
-// };
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};
 
-// void insertathead(Node *&head, int data)
-// {
-//     // create new node
-//     Node *newnode = new Node(data);
-//     // attach newnode to the head
-//     newnode->next = head;
-//     // now head is new node
-//     head = newnode;
-// }
-// void insertatend(Node *&head, int data)
-// {
-//     // create new node
-//     Node *newnode = new Node(data);
-//     if(head==NULL){
-//         head->next=newnode;
-//         head=newnode;
-//     }
-//     else{
-//         Node* temp = head;
+void insertathead(Node *&head, int data)
+{
+    // create new node
+    Node *newnode = new Node(data);
+    // attach newnode to the head
+    newnode->next = head;
+    // now head is new node
+    head = newnode;
+}
+void insertatend(Node *&head, int data)
+{
+    // create new node
+    Node *newnode = new Node(data);
+    if(head==NULL){
+        head->next=newnode;
+        head=newnode;
+    }
+    else{
+        Node* temp = head;
 
-//         while(temp->next!=NULL){
-//             temp=temp->next;
-//         }
-//         temp->next=newnode;
-//     }
+        while(temp->next!=NULL){
+            temp=temp->next;
+        }
+        temp->next=newnode;
+    }
 
-// }
+}
 
-// void print(Node *head)
-// {
-//     Node *temp = head;
-//     while (temp != NULL)
-//     {
-//         cout << temp->data << " ";
-//         temp = temp->next;
-//     }
-//     cout << endl;
-// }
+void print(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
 
-// int main()
-// {
-//     Node *head =  NULL;
-//     Node *tail = NULL;
-//     Node *first = new Node(10);
-//     Node *second = new Node(20);
-//     Node *third = new Node(30);
-//     Node *forth = new Node(40);
-//     Node *fifth = new Node(50);
-//     Node *sixth = new Node(60);
-//     Node *seventh = new Node(70);
+int main()
+{
+    Node *head =  NULL;
+    Node *tail = NULL;
+    Node *first = new Node(10);
+    Node *second = new Node(20);
+    Node *third = new Node(30);
+    Node *forth = new Node(40);
+    Node *fifth = new Node(50);
+    Node *sixth = new Node(60);
+    Node *seventh = new Node(70);
 
-//     first->next = second;
-//     second->next = third;
-//     third->next = forth;
-//     forth->next = fifth;
-//     fifth->next = sixth;
-//     sixth->next = seventh;
+    first->next = second;
+    second->next = third;
+    third->next = forth;
+    forth->next = fifth;
+    fifth->next = sixth;
+    sixth->next = seventh;
 
-//     insertathead(first, 100);
-//     insertatend(first, 100);
+    insertathead(first, 100);
+    insertatend(first, 100);
 
-//     cout << "printing LL:";
-//     print(first);
-// }
+    cout << "printing LL:";
+    print(first);
+}
 
-////INSERT at STAT AND END
-// #include <iostream>
-// using namespace std;
+//INSERT at STAT AND END
+#include <iostream>
+using namespace std;
 
-// class Node
-// {
-// public:
-//     int data;
-//     Node *next;
+class Node
+{
+public:
+    int data;
+    Node *next;
 
-//     Node()
-//     {
-//         this->data = 0;
-//         this->next = NULL;
-//     }
+    Node()
+    {
+        this->data = 0;
+        this->next = NULL;
+    }
 
-//     Node(int data)
-//     {
-//         this->data = data;
-//         this->next = NULL;
-//     }
-// };
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};
 
-// void insertathead(Node *&head,Node *&tail, int data)
-// {
-//     if (head == NULL)
-//     {
-//         Node *newnode = new Node(data);
-//         head = newnode;
-//         tail = newnode;
-//         return;
-//     }
+void insertathead(Node *&head,Node *&tail, int data)
+{
+    if (head == NULL)
+    {
+        Node *newnode = new Node(data);
+        head = newnode;
+        tail = newnode;
+        return;
+    }
 
-//     // create new node
-//     Node *newnode = new Node(data);
-//     // attach newnode to the head
-//     newnode->next = head;
-//     // now head is new node
-//     head = newnode;
-// }
-// void insertattail(Node *&head, Node *&tail, int data)
-// {
-//     if (head == NULL)
-//     {
-//         Node *newnode = new Node(data);
-//         head = newnode;
-//         tail = newnode;
-//         return;
-//     }
-//     // create node
-//     Node *newnode = new Node(data);
-//     // connect with new node
-//     tail->next = newnode;
-//     // update tail
-//     tail = newnode;
-// }
+    // create new node
+    Node *newnode = new Node(data);
+    // attach newnode to the head
+    newnode->next = head;
+    // now head is new node
+    head = newnode;
+}
+void insertattail(Node *&head, Node *&tail, int data)
+{
+    if (head == NULL)
+    {
+        Node *newnode = new Node(data);
+        head = newnode;
+        tail = newnode;
+        return;
+    }
+    // create node
+    Node *newnode = new Node(data);
+    // connect with new node
+    tail->next = newnode;
+    // update tail
+    tail = newnode;
+}
 
-// void print(Node *head)
-// {
-//     Node *temp = head;
-//     while (temp != NULL)
-//     {
-//         cout << temp->data << " ";
-//         temp = temp->next;
-//     }
-//     cout << endl;
-// }
+void print(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
 
-// int main()
-// {
-//     Node *head = NULL;
-//     Node *tail = NULL;
+int main()
+{
+    Node *head = NULL;
+    Node *tail = NULL;
 
-//     insertathead(head, tail, 20);
+    insertathead(head, tail, 20);
 
-//     insertattail(head, tail, 110);
-//     insertathead(head, tail, 30);
+    insertattail(head, tail, 110);
+    insertathead(head, tail, 30);
 
-//     insertattail(head, tail, 100);
+    insertattail(head, tail, 100);
 
-//     cout << "printing LL:";
-//     print(head);
-// }
+    cout << "printing LL:";
+    print(head);
+}
 
 // INSERT AT ANY POSITION
 #include <iostream>
