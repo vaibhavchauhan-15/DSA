6037bd6b04abecec755576251de566ca394ad798
VAIBHAV CHAUHAN <123004674+UDAAKADAL@users.noreply.github.com>
2024-06-15 13:37:53 +0530

    Update 2d_array.cpp

--- 2d_array.cpp
+++ 2d_array.cpp
@@ -10,7 +10,7 @@ int main(){
 //initialize  
 int arr[3][3]={ {1,2,3},{4,5,6},{7,8,9}};
 //printing
-for(int i=0;i<3;i++){
+for(int i=0;i<3;i++){ 
    for(int j=0;j<3;j++){
        cout<<arr[i][j]<<" "; 
    } 
