872c0d7fd9b9d4dfbf6d9e3bf5d602804b7cb773
vaibhavchauhan-15 <vaibhavchauhan12353@gmail.com>
2025-05-10 22:46:16 +0530

    Add print function to display array elements and remove size output

--- ARRAYS/dynamicarr.cpp
+++ ARRAYS/dynamicarr.cpp
@@ -1,8 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
+void print(int arr[] ,  int n){
+    for(int i=0;i<n;i++){
+        cout<<i<<" ";
+    }
+}
 int main(){
     int n;
     cin>>n;
     int * arr=new int [n];
-    cout<<"Size is :"<<sizeof(arr);
+    // cout<<"Size is :"<<sizeof(arr);
+    print(arr , n);
 }
