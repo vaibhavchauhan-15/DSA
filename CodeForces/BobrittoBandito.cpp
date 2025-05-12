789b81848d136ac726698a7e2f8dd3746d5126df
vaibhavchauhan-15 <vaibhavchauhan12353@gmail.com>
2025-05-12 21:49:18 +0530

    Fix formatting issues and correct logic in BobrittoBandito program

--- CodeForces/BobrittoBandito.cpp
+++ CodeForces/BobrittoBandito.cpp
@@ -2,35 +2,28 @@
 using namespace std;
 
 int main()
-
 {
     long long tt;
     cin >> tt;
-
     while (tt--)
     {
         int n, m, r, l;
-
         cin >> n >> m >> l >> r;
-        int r1 = 0, l1
-        
+        int r1 = 0, l1 = 0;
         for (int i = 0; i <= r; i++)
         {
-            if (m >= 1 && r1 + 1 <
-                = r)
+            if (m >= 1 && r1 + 1 <= r)
             {
-                r1++; 
+                r1++;
 
                 m--;
             }
         }
-
         for (int i = l; i <= 0; i++)
         {
             if (m >= 1)
             {
                 l1--;
-                
                 m--;
             }
         }
