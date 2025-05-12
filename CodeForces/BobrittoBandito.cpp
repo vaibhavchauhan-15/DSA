5f123b7b92ff1551ca4d8309bdb25e2878bb825b
vaibhavchauhan-15 <vaibhavchauhan12353@gmail.com>
2025-05-12 21:47:56 +0530

    Fix formatting issues and correct variable initialization in BobrittoBandito program

--- CodeForces/BobrittoBandito.cpp
+++ CodeForces/BobrittoBandito.cpp
@@ -2,19 +2,24 @@
 using namespace std;
 
 int main()
+
 {
     long long tt;
     cin >> tt;
+
     while (tt--)
     {
         int n, m, r, l;
+
         cin >> n >> m >> l >> r;
-        int r1 = 0, l1 = 0;
+        int r1 = 0, l1
+         = 0;
         for (int i = 0; i <= r; i++)
         {
             if (m >= 1 && r1 + 1 <= r)
             {
-                r1++;
+                r1++; 
+
                 m--;
             }
         }
