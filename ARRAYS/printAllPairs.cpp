2184a9d0b31bbf351d2d47022b3873244d379cdc
vaibhavchauhan-15 <vaibhavchauhan12353@gmail.com>
2025-05-13 20:06:25 +0530

    Refactor printAllPairs program to remove commented-out code and improve triplet pair output

--- ARRAYS/printAllPairs.cpp
+++ ARRAYS/printAllPairs.cpp
@@ -3,25 +3,35 @@ using namespace std;
 int main(){
     int arr[]={1,5,8,5,7,8};
     int n=6;
-    cout<<"Double pairs "<<endl;
-    for (int i = 0; i < n; i++) {
-        for (int j =0; j < n; j++){
+    // cout<<"Double pairs "<<endl;
+    // for (int i = 0; i < n; i++) {
+    //     for (int j =0; j < n; j++){
 
-            cout<<arr[i]<<","<<arr[j]<< " ";  
-        }
-        cout<<endl;
-    }
+    //         cout<<arr[i]<<","<<arr[j]<< " ";  
+    //     }
+    //     cout<<endl;
+    // }
 
-    //reverse
-    cout<<"Reversed "<<endl;
-    for (int i = 0; i < n; i++) {
-        for (int j =5 ; j >= 0; j--){
+    // //reverse
+    // cout<<"Reversed "<<endl;
+    // for (int i = 0; i < n; i++) {
+    //     for (int j =5 ; j >= 0; j--){
 
-            cout<<arr[i]<<","<<arr[j]<< " ";  
-        }
-        cout<<endl;
-    }
+    //         cout<<arr[i]<<","<<arr[j]<< " ";  
+    //     }
+    //     cout<<endl;
+    // }
     cout<<"Triplet Pairs"<<endl;
-    
+    for (int i = 0; i < n; i++) {
+        for (int j =0 ; j < n; j++){
+            for (int k = 0; k < n; k++) {
+                
+                cout<<arr[i]<<" ,"<<arr[j]<< " , " <<arr[k]<<" ";  
+            }
+
+        }
+        cout<<endl;
+    }
+
     return 0;
 }
