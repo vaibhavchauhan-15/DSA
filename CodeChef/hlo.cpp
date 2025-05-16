58ca777ff1494e4cc8a0cb4cb48c2edceef67b90
vaibhavchauhan-15 <vaibhavchauhan12353@gmail.com>
2025-05-16 20:22:00 +0530

    Remove hlo.cpp and add new file LongestSubsequence.cpp

--- CodeChef/hlo.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-int main()
-{
-  int n = 5;
-  float divide = (float)n / 10;  // Cast to float for floating-point division
-  cout << divide;  // Will now output 0.5
-  return 0;
-}
