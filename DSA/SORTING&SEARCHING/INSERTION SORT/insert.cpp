8f03d7ea79f8a2abbc38d1b31dfbbda0846daeb9
vaibhavchauhan-15 <vaibhavchauhan12353@gmail.com>
2025-05-19 09:45:59 +0530

    Refactor insertion sort implementation for clarity; add detailed comments and improve code readability

--- DSA/SORTING&SEARCHING/INSERTION SORT/insert.cpp
+++ DSA/SORTING&SEARCHING/INSERTION SORT/insert.cpp
@@ -5,13 +5,15 @@ void insertionSort(vector<int>&arr){
 //left me jitne bhi element hai usase compare karo
 //aur key value ko usako jagah pe rakh do
     int n=arr.size();
-    for (int i = 1; i < n; i++) {//n-1 times comparison
-        int key=arr[i];
+    for (int i = 1; i < n; i++) {//i ko 1 se n-1 kar chalao
+        int key=arr[i];//store key
         int j=i-1;
         while(j>=0 && arr[j]>key){
-            arr[j+1]=arr[j];
+            arr[j+1]=arr[j];//jo bhi greater than key hai 
+            j--;            //usako right me shift kar do
         }
-    }
+        arr[j+1]=key;//jo key tha  usako sabse nearest greater 
+    }                // k pahle rakh do
     //print sorted array
     for (int i = 0; i < n; i++) {
         cout <<arr[i]<< " ";
