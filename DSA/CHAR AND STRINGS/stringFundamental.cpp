d5cec504d2eca1f691d2b10ddb63e7357dfcf3f0
vaibhavchauhan-15 <vaibhavchauhan12353@gmail.com>
2025-05-22 09:05:56 +0530

    Refactor stringFundamental.cpp to improve output handling; uncomment relevant print statements for better user feedback

--- DSA/CHAR AND STRINGS/stringFundamental.cpp
+++ DSA/CHAR AND STRINGS/stringFundamental.cpp
@@ -9,8 +9,17 @@ int main(){
     // cin>>name;
     getline(cin , name);
     //print
-    cout<<name<<endl;
+    // cout<<name<<endl;
     // cout << "First letter of name is :" <<name[0]<<endl;
+    
+    //find lenght
+    cout << "Lengh of string is : "<<name.length() <<endl;
+
+    //check empty or not
+    cout << "String is empty or not(0 =false | 1 = true) : "<<name.empty() <<endl;
+
+    //char at position
+    cout << "Character at 0 position is : "<<name.at(0) <<endl;
 
     return 0;
 }
