913c98a0ffc289abc003b9d4d86d85a9e72ac4d3
vaibhavchauhan-15 <vaibhavchauhan12353@gmail.com>
2025-05-22 10:11:34 +0530

    Refactor stringFundamental.cpp to uncomment and enhance string manipulation examples; add substring functionality

--- DSA/CHAR AND STRINGS/stringFundamental.cpp
+++ DSA/CHAR AND STRINGS/stringFundamental.cpp
@@ -2,16 +2,13 @@
 #include<string>
 using namespace std;
 int main(){
-    cout << "Enter name :";
-    //creation
-    string name;
-    //input
-    // cin>>name;
-    getline(cin , name);
-    string Lname;
-    cout << "Enter last name :";
-    getline(cin , Lname);
-    //print
+    // cout << "Enter name :";
+    // //creation
+    // string name;
+    // //input
+    // // cin>>name;
+    // getline(cin , name);
+    // //print
     // cout<<name<<endl;
     // cout << "First letter of name is :" <<name[0]<<endl;
     
@@ -27,11 +24,65 @@ int main(){
     // cout << "Front char is : "<<name.front() <<endl;
     // cout << "Last char is :"<<name.back() <<endl;
 
-    cout << "Before contacatinate "<<endl<<"Name :"<<name<<endl<<"Last Name : "<<Lname <<endl;
 
-    //containate two string (append)
-    name.append(Lname);
-    cout << "After contacatinate :"<<name <<endl;
+    ////append
+    // string str1="vaibhav";
+    // string str2="chauhan";
 
-    // return 0;
+    // cout << "Before contacatinate "<<endl<<"Name :"<<str1<<endl<<"Last Name : "<<str2 <<endl;
+
+    // //containate two string (append)
+    // str1.append(str2);
+    // cout << "After contacatinate "<<endl<<str1 <<endl<<str2<<endl;
+
+
+    //erase
+    // string str1,str2;
+    // str1="vaibhav";
+    // cout << "before erase :"<<str1 <<endl;
+    // str1.erase(2,4);//it erase from index 2 - 4+1
+    // cout << "After erase :"<<str1 <<endl;
+
+    // //insert
+    // string str1,middle;
+    // str1="vaibhav";
+    // middle = "Chauhan";
+    // //inserting
+    // str1.insert(4,middle);//insert after index 4
+    // cout << "After insering : "<<str1 <<endl;
+    ////push_back and pop_back
+
+//     string str1="vaibahv";
+//     str1.push_back('C');//add at end
+//     cout<<str1<<endl;
+//     str1.pop_back();//remove from end
+//     cout<<str1<<endl;
+
+    // //find string
+    // string s,t;
+    // s="yar tera super star desi kalakaar ";
+    // t="star";
+    // if(s.find(t)==string::npos){//npos return true if not found
+    //     cout << "NOt found" <<endl;
+    // }else{
+    //     cout << "FOund" <<endl;
+    // }
+
+    // // compare string
+    // string s,t;
+    // s="vaibhav";
+    // t="vaibhav";
+
+    // if(s.compare(t)==0){//o means mached
+    //     cout << "Maching" <<endl;
+    // }else{
+    //     cout << "Not Matching" <<endl;
+    // }
+
+
+    //******sub string**************** */
+    string name="vaibahv chauhan";
+    cout << "Sub String of name from 3  index and lengh is 9 is : "<<name.substr(3,9) <<endl;
+    
+    return 0;
 }
