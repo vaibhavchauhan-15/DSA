b63707b897e9b64cfbab05bd8b7a42126aea647e
vaibhavchauhan-15 <vaibhavchauhan12353@gmail.com>
2025-05-22 09:12:42 +0530

    Refactor stringFundamental.cpp to improve input handling; streamline output and enhance user interaction

--- DSA/CHAR AND STRINGS/stringFundamental.cpp
+++ DSA/CHAR AND STRINGS/stringFundamental.cpp
@@ -8,18 +8,30 @@ int main(){
     //input
     // cin>>name;
     getline(cin , name);
+    string Lname;
+    cout << "Enter last name :";
+    getline(cin , Lname);
     //print
     // cout<<name<<endl;
     // cout << "First letter of name is :" <<name[0]<<endl;
     
-    //find lenght
-    cout << "Lengh of string is : "<<name.length() <<endl;
+    // //find lenght
+    // cout << "Lengh of string is : "<<name.length() <<endl;
 
-    //check empty or not
-    cout << "String is empty or not(0 =false | 1 = true) : "<<name.empty() <<endl;
+    // //check empty or not
+    // cout << "String is empty or not(0 =false | 1 = true) : "<<name.empty() <<endl;
 
-    //char at position
-    cout << "Character at 0 position is : "<<name.at(0) <<endl;
+    // //char at position
+    // cout << "Character at 0 position is : "<<name.at(0) <<endl;
 
-    return 0;
+    // cout << "Front char is : "<<name.front() <<endl;
+    // cout << "Last char is :"<<name.back() <<endl;
+
+    cout << "Before contacatinate "<<endl<<"Name :"<<name<<endl<<"Last Name : "<<Lname <<endl;
+
+    //containate two string
+    name.append(Lname);
+    cout << "After contacatinate :"<<name <<endl;
+
+    // return 0;
 }
