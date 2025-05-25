be591acb5a82be0d0fd0b5324d551ee7512224e8
vaibhavchauhan-15 <vaibhavchauhan12353@gmail.com>
2025-05-25 20:50:22 +0530

    Add segmented sieve implementation and update types documentation

---
+++ DSA/4 BASIC MATH FOR DSA/segmentSieve.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+void simpleSieve(int limit, vector<int> &prime) {
+    vector<bool> mark(limit + 1, true);
+    mark[0] = mark[1] = false;
+
+    for (int i = 2; i <= limit; i++) {
+        if (mark[i]) {
+            prime.push_back(i);
+            for (int j = i * i; j <= limit; j += i)
+                mark[j] = false;
+        }
+    }
+}
+
+void segmentedSieve(long long L, long long R) {
+    vector<int> prime;
+    simpleSieve(sqrt(R), prime);
+
+    int n = R - L + 1;
+    vector<bool> isPrime(n, true);
+
+    for (int p : prime) {
+        long long start = max(1LL * p * p, (L + p - 1) / p * p);
+        for (long long j = start; j <= R; j += p)
+            isPrime[j - L] = false;
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (isPrime[i] && (L + i) != 1)
+            cout << (L + i) << " ";
+    }
+}
+
+int main(){
+    
+    return 0;
+}
+
