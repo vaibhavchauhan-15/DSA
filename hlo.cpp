95bb61c6f12e22cc39e1daa363f7aa5f95d1fa80
vaibhavchauhan-15 <vaibhavchauhan12353@gmail.com>
2025-05-27 22:24:17 +0530

    Refactor solution class and remove obsolete sieve function and executable file

--- hlo.cpp
+++ hlo.cpp
@@ -1,31 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
-void seive(int n){
-    vector<bool>prime(n ,true);
-    prime[0]=prime[1]=false;
-    for (int i = 2; i < n; i++) {
-        if(prime[i]==true){
-            int j=i*2;
-            while(j<n){
-                prime[j]=false;
-                j=j+i;
+class Solution {
+public:
+    int differenceOfSums(int n, int m) {
+        int num1=0; //not divisible by m
+        int num2=0;//divisible by m
+        
+        for (int i = 1; i <= n; i++) {
+            if(i%m==0){
+                num2+=i;
             }
+            else{
+                
+                num1+=i;
+            }  
         }
+        // cout<<"Num 1 :"<<num1<<endl;
+        // cout<<"Num 2 :"<<num2<<endl;
+        return num1 - num2;
     }
-    cout << "Prime number are :";
-    int count=0;
-    for (int i = 0; i < n; i++) {
-        if(prime[i]){
-            count++;
-            cout<<i<<" ";
-        }
-    }
-    cout << "" <<endl;
-    cout << "Total Prime Number from 0 to "<<n<<" is :"<<count <<endl;
-}
+};
 int main(){
-    cout << "Enter Number :";
-    int n;cin>>n;
-    seive(n);
+    int n ,m;
+    cin>>n>>m;
+    Solution sc;
+    cout<<sc.differenceOfSums(n,m);
     return 0;
 }
