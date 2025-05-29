e8f5010cd0c6adcbd12569beb1deb0ff608b6a72
vaibhavchauhan-15 <vaibhavchauhan12353@gmail.com>
2025-05-29 16:07:27 +0530

    Refactor pointerOfPointer.cpp for clarity and consistency; update pointerOfPointer.exe

--- DSA/5 POINTERS/pointerOfPointer.cpp
+++ DSA/5 POINTERS/pointerOfPointer.cpp
@@ -6,7 +6,12 @@ int main(){
     int** ptr1=&ptr;//pointer of pointer or double pointer it store the address of ptr
     int*** ptr2=&ptr1;//tripple pointer it store the address of ptr1
     int**** ptr3=&ptr2;//fourth pointer it store the address of ptr2
-    cout<<* ptr3<<endl;//it print the address of ptr2
+
+    
+
+
+    cout<<*** ptr3<<endl;//it print the address of ptr2
     cout<< ptr2<<endl;//it print own address
+    
     return 0;
 }
