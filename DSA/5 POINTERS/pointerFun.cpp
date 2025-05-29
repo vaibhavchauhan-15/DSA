c473dbacab421e160dc3ba294b848b97dd3ccb2c
vaibhavchauhan-15 <vaibhavchauhan12353@gmail.com>
2025-05-29 15:58:00 +0530

    Refactor code structure for improved readability and maintainability

--- DSA/5 POINTERS/pointerFun.cpp
+++ DSA/5 POINTERS/pointerFun.cpp
@@ -1,6 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+void solve(int arr[],int size){
+    cout << "Inside solve arr -> :"<<arr <<endl;
+    cout << "Inside solve &arr -> :"<<&arr <<endl;//it print the addres of address
+    cout << "Inside solve &arr[0] -> :"<<&arr[0] <<endl;  
+}
+
 int main(){
-    
+    int arr[5]={10,20,30,40,50};
+
+    cout << "Inside main arr -> :"<<arr <<endl;
+    cout << "Inside main &arr -> :"<<&arr <<endl;
+    cout << "Inside main &arr[0] -> :"<<&arr[0] <<endl;
+
+    solve(arr,5);
     return 0;
 }
