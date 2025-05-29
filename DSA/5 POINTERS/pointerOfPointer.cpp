bf1ef317f5cd618a798036172b68525e183778d0
vaibhavchauhan-15 <vaibhavchauhan12353@gmail.com>
2025-05-29 16:13:12 +0530

    Format pointerOfPointer.cpp for consistency and readability; update pointerOfPointer.exe

--- DSA/5 POINTERS/pointerOfPointer.cpp
+++ DSA/5 POINTERS/pointerOfPointer.cpp
@@ -1,18 +1,21 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
-    int a=8;
-    int* ptr=&a;//pointer
-    int** ptr1=&ptr;//pointer of pointer or double pointer it store the address of ptr
-    int*** ptr2=&ptr1;//tripple pointer it store the address of ptr1
-    int**** ptr3=&ptr2;//fourth pointer it store the address of ptr2
+int main()
+{
+    int a = 8;
+    int *ptr = &a;        // pointer
+    int **ptr1 = &ptr;    // pointer of pointer or double pointer it store the address of ptr
+    int ***ptr2 = &ptr1;  // tripple pointer it store the address of ptr1
+    int ****ptr3 = &ptr2; // fourth pointer it store the address of ptr2
 
-cout << "Printing all pointer address" <<endl; 
-cout << "ptr :"  <<endl;
+    cout << "Printing all pointer address" << endl;
+    cout << "ptr :" << ptr << endl;
+    cout << "ptr1 :" << ptr1 << endl;
+    cout << "ptr2 :" << ptr2 << endl;
+    cout << "ptr3 :" << ptr3 << endl;
 
+    cout << ***ptr3 << endl; // it print the address of ptr2
+    cout << ptr2 << endl;    // it print own address
 
-    cout<<*** ptr3<<endl;//it print the address of ptr2
-    cout<< ptr2<<endl;//it print own address
-    
     return 0;
 }
