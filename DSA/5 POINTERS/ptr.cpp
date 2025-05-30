d46fea5d7b164c5cd9cad1faf1016386bdd504d3
vaibhavchauhan-15 <vaibhavchauhan12353@gmail.com>
2025-05-30 18:56:29 +0530

    Add ptr.cpp to demonstrate pointer manipulation and address handling; implement solve3 function for reference passing; update ptr.exe Add fact.cpp to implement factorial calculation using recursion; add fact.exe

--- DSA/5 POINTERS/ptr.cpp
+++ DSA/5 POINTERS/ptr.cpp
@@ -1,11 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void solve1(int* p){
+void solve1(int* p){//pass by value
     *p+=5;//it change the original values
 }
-void solve2(int* p){
+void solve2(int* p){//pass by value
     p+=5;//it make copy of p and assign the new address 
+    cout << p <<endl;
+}
+void solve3(int* &p){//pass by refrence
+    p+=5;//it change the original address of p
     cout << p <<endl;
 }
 int main(){
@@ -28,5 +32,12 @@ int main(){
     cout << p <<endl;
     cout << &p <<endl;
     cout << *p <<endl;
+
+    cout << "after solve3 call" <<endl;
+    solve3(p);
+
+    cout << p <<endl;
+    cout << &p <<endl;
+    cout << *p <<endl;//here it give garbage value or runtime error
     return 0;
 }
---
+++ DSA/6 RECURSION/fact.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int fact(int n){
+    int ans;
+    if(n>=1){//base case
+        ans= n * fact(n-1);
+    }
+    return ans;
+}
+
+int main(){
+    int n=5;
+    cout<<fact(n)<<endl;
+    cout<<5*4*3*2*1;
+    return 0;
+}
