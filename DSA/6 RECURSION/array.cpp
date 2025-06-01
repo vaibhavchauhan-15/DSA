c985456cb142cd5e847336956dac712a47b03f1d
vaibhavchauhan-15 <vaibhavchauhan12353@gmail.com>
2025-06-01 20:26:32 +0530

    Add recursive functions for printing an array, calculating sum, and climbing stairs; include corresponding executables

--- DSA/6 RECURSION/array.cpp
+++ DSA/6 RECURSION/array.cpp
@@ -10,7 +10,7 @@ void printVector(vector<int>arr,int n){
     printVector(arr,n-1);
     cout<<arr[n]<<" ";
 }   
-
+ 
 int main(){
     vector<int>arr ={10,20,30,40,50,60,70,80,90,100};
     int n=arr.size()-1;
