c1e977cef7c4468ed53d90338e8d51a79b0dadff
vaibhavchauhan-15 <vaibhavchauhan12353@gmail.com>
2025-06-02 21:31:45 +0530

    class Solution { public:     int candy(vector<int> &ratings)     {         int n = ratings.size();         vector<int> candy(n, 1);         for (int i = 0; i < n; i++)         {             if (i + 1 <=n && ratings[i] > ratings[i + 1] )             {                 candy[i] += 1;             }else if(i - 1 >=0 && ratings[i] > ratings[i - 1]){                 candy[i] += 1;             }         }         int totalCandy = 0;         for (int i = 0; i < n; i++)         {             totalCandy += candy[i];         }         return totalCandy;     } };

--- DSA/1 ARRAYS/leetcode3.cpp
+++ DSA/1 ARRAYS/leetcode3.cpp
@@ -1,16 +1,35 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int candy(vector<int>& ratings) {
-        
+    int candy(vector<int> &ratings)
+    {
+        int n = ratings.size();
+        vector<int> candy(n, 1);
+        for (int i = 0; i < n; i++)
+        {
+            if (i + 1 <=n && ratings[i] > ratings[i + 1] )
+            {
+                candy[i] += 1;
+            }else if(i - 1 >=0 && ratings[i] > ratings[i - 1]){
+                candy[i] += 1;
+            }
+        }
+        int totalCandy = 0;
+        for (int i = 0; i < n; i++)
+        {
+            totalCandy += candy[i];
+        }
+        return totalCandy;
     }
 };
 
-int main(){
-    vector<int> ratings={1,2,2};
+int main()
+{
+    vector<int> ratings = {1,2,2};
     Solution sc;
-    cout << "Total candy is :"<<sc.candy(ratings) <<endl;
+    cout << "Total candy is :" << sc.candy(ratings) << endl;
     return 0;
 }
