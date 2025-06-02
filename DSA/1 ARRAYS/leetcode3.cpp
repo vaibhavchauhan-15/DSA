fd821d4a82c3127c8d8ba9f9cb038f6899ce990e
vaibhavchauhan-15 <vaibhavchauhan12353@gmail.com>
2025-06-02 21:16:38 +0530

    Add leetcode3.cpp to implement candy distribution logic

---
+++ DSA/1 ARRAYS/leetcode3.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        
+    }
+};
+
+int main(){
+    vector<int> ratings={1,2,2};
+    Solution sc;
+    cout << "Total candy is :"<<sc.candy(ratings) <<endl;
+    return 0;
+}
