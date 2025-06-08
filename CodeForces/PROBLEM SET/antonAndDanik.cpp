1ccd3f7050ad9abd4956fee4c1d32e95c2b3bc6e
vaibhavchauhan-15 <vaibhavchauhan12353@gmail.com>
2025-06-08 22:12:19 +0530

    Refactor code structure for improved readability and maintainability

--- CodeForces/PROBLEM SET/antonAndDanik.cpp
+++ CodeForces/PROBLEM SET/antonAndDanik.cpp
@@ -5,5 +5,13 @@ int main(){
     string s;cin>>s;
     int countA=0;
     int countD=0;
+    for(char ch : s){
+        if( ch == 'A') countA++;
+        if( ch == 'D') countD++;
+
+    }
+    if(countA==countD) cout<<"Friendship";
+    else if(countA>countD) cout<<"Anton";
+    else cout<<"Danik";
     return 0;
 }
