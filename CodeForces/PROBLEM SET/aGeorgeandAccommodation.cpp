aaa9ae617ead6be83ab14eb8529107582a6aa517
vaibhavchauhan-15 <vaibhavchauhan12353@gmail.com>
2025-06-16 22:43:57 +0530

    Add aGeorgeandAccommodation program to count suitable accommodations for George and Alex

---
+++ CodeForces/PROBLEM SET/aGeorgeandAccommodation.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    
+    int count = 0;
+    for (int i = 0; i < n; i++) {
+        int p, q;
+        cin >> p >> q;
+        
+        // Check if there's space for at least 2 more people (George and Alex)
+        if (q - p >= 2) {
+            count++;
+        }
+    }
+    
+    cout << count << endl;
+    return 0;
+}
