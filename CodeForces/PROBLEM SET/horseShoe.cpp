062d12afd9c6ec93cac62a334fc46b6336a4b1d5
vaibhavchauhan-15 <vaibhavchauhan12353@gmail.com>
2025-06-22 22:01:42 +0530

    Implement initial structure for dividing a string into groups of size k with fill character

---
+++ CodeForces/PROBLEM SET/horseShoe.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int s1,s2,s3,s4 ;cin>>s1>>s2>>s3>>s4;
+    
+    int count=0;
+    if(s1==s2 || s1==s3 || s1==s4){
+        count++;
+    }
+    if(s2==s3 || s2==s4){
+        count++;
+    }
+    if(s3==s4){
+        count++;
+    }
+    
+    cout << count <<endl;
+    return 0;
+}
