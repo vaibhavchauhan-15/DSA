6e5e2194882c63e54edfadbcdeecc1989851cd0c
vaibhavchauhan-15 <vaibhavchauhan12353@gmail.com>
2025-06-23 21:30:56 +0530

    Add initial implementation of Student class with study method

---
+++ DSA/9 OOPS/class1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+class Student
+{
+public:
+    int id;
+    // string name;
+    // int age;
+    // string course;
+
+    void study()
+    {
+        cout << "Studying" << endl;
+    }
+    // void exam(){
+    //     cout << "Giving Exam" <<endl;
+    // }
+    // void play(){
+    //     cout << "Playing" <<endl;
+    // }
+};
+int main()
+{
+    Student s1;
+    s1.study();
+    Student s2;
+
+    cout << sizeof(Student) << endl;
+    return 0;
+}
