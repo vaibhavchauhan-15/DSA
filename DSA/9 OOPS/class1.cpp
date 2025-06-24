2f240fcc8d2b1a70774571ea4b18e5f7f28cf304
vaibhavchauhan-15 <vaibhavchauhan12353@gmail.com>
2025-06-24 18:32:46 +0530

    Refactor Student class: reorganize properties and methods, enhance constructor initialization

--- DSA/9 OOPS/class1.cpp
+++ DSA/9 OOPS/class1.cpp
@@ -3,29 +3,63 @@ using namespace std;
 
 class Student
 {
-public:
-    int id;
-    // string name;
-    // int age;
-    // string course;
 
+//access modifier
+private:
+//properties
+string gf;
+
+//access modifier
+public:
+    //properties
+    int id;
+    string name;
+    int age;
+    string course;
+
+    //defaul constructor(ctor) by default assign garbage value
+    Student(){
+        // cout << "Default ctor called" <<endl;
+    }
+
+    //parametrize ctor
+    Student(int _id , string _name , int _age , string _course , string _gf ){
+        // cout << "Parametrize ctor called" <<endl;
+        id=_id;
+        name=_name;
+        age=_age;
+        course=_course;
+        gf=_gf;
+
+    }
+    //behaviour
     void study()
     {
         cout << "Studying" << endl;
     }
-    // void exam(){
-    //     cout << "Giving Exam" <<endl;
-    // }
-    // void play(){
-    //     cout << "Playing" <<endl;
-    // }
+    void exam(){
+        cout << "Giving Exam" <<endl;
+    }
+    void play(){
+        cout << "Playing" <<endl;
+    }
+
+private:
+    void gfChatting(){
+        cout << "Chatting with gf ." <<endl;
+    }
 };
 int main()
 {
-    Student s1;  
-    s1.study();
-    Student s2;    
+    Student s1; 
+    s1.id=1;
+    s1.name="vaibhav";
+    s1.age=20;
+    s1.course=6;
+    cout<<s1.name<<endl;
+    Student s2(2,"aman",22,"b.tech","amani");    
+    cout << s2.name <<endl;
 
-    cout << sizeof(Student) << endl;
+    // cout << sizeof(Student) << endl;//size of the class
     return 0;
 }
