774048d9f5b69bcd23b77681bb935caf6a205c77
VAIBHAV CHAUHAN <vaibhavchauhan12353@gmail.com>
2025-07-14 22:36:52 +0530

    Add implementation for counting possible strings in a given word

---
+++ LEETCODE/PROBLEM SET/3330.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+    int possibleStringCount(string word) {
+        int count=1;
+        for (int i = 0; i < word.size(); i++) {
+            if(i <word.size() && i-1 >= 0 && word[i]==word[i-1] ){
+                count++;
+            }
+        }
+        return count;
+    }
+};
+int main(){   
+    string word="aaaa";
+    Solution sc;
+    cout<<sc.possibleStringCount(word)<<endl;
+    return 0;
+}
