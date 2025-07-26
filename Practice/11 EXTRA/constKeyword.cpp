7b39a28d67d8d9fbf1fe3445cb74bac709760e2d
vaibhavchauhan-15 <vaibhavchauhan12353@email.com>
2025-07-26 16:24:22 +0530

    const keyword

---
+++ Practice/11 EXTRA/constKeyword.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    //const data // non const pointer
+
+    cout << "const data and non const pointer" << endl;
+    int const * x = new int(10);//const data
+    // *x=20;//cant change
+    int y=20;
+    cout <<"Before : "<< *x << endl;
+    x=&y;//change the pointer
+    cout << "After : "<<*x << endl;
+    
+    
+    //non const data // const pointer
+    cout << "const data and non const pointer" << endl;
+    int  * const y = new int(100);//const pointer
+
+    return 0;
+}
