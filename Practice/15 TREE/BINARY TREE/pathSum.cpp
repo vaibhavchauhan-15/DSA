b3264768f6dc0bd4070eb1572a8cea4f47b0d396
vaibhavchauhan-15 <vaibhavchauhan12353@email.com>
2025-07-31 22:41:07 +0530

    Refactor pathSum.cpp to include Node class and createTree function

--- Practice/15 TREE/BINARY TREE/pathSum.cpp
+++ Practice/15 TREE/BINARY TREE/pathSum.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
 using namespace std;
 
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val){
+        this->data=val;
+        this->left=nullptr;
+        this->right=nullptr;
+    }
+};
+
+
+
+Node* createTree(){
+    int val;cin>>val;
+
+    if(val==-1){
+        return nullptr;
+    }
+
+    Node* root = new Node(val);
+
+    root->left=createTree();
+    root->right=createTree();
+
+    return root;
+}
 int main() {
     
     return 0;
